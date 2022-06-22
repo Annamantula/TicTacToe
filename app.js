365ed7e7d279ad016ad00ded7576d82109797a84
Anna Mantula <ganna.mantula@gmail.com>
2022-06-22 15:27:02 -0500

    start html & css for space-ticTacToe

---
+++ app.js
@@ -0,0 +1,15 @@
+let board = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+];
+
+function canvas (){createCanvas(500,500);}
+let player 1 = 'X';
+let player 2 = 'O';
+
+for (let i = 0; i<3; i++){
+    for ( let j =0; j<3; j++){
+
+    }
+} 
